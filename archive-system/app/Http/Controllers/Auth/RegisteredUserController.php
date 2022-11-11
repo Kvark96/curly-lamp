ced96631ccb1dcf4bc146faf6129312d18e551a0
atombomben <71074932+atombomben@users.noreply.github.com>
2022-11-11 14:01:22 +0100

    Register function is now configured to be user only

--- archive-system/app/Http/Controllers/Auth/RegisteredUserController.php
+++ archive-system/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,6 +44,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'type_id' => 3
         ]);
 
         event(new Registered($user));
