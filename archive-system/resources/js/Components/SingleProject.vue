fb74d745f1b9a5fe4409259aa324b5714296eec8
atombomben <71074932+atombomben@users.noreply.github.com>
2022-11-28 23:09:40 +0100

    Frontend on singleproject updated
    Structure editted a bit

--- archive-system/resources/js/Components/SingleProject.vue
+++ archive-system/resources/js/Components/SingleProject.vue
@@ -1,26 +1,47 @@
 <template>
     <div class="">
         <div class="flex items-center justify-center">
-            <div class="w-full px-2 py-4">
+            <div class="w-full px-2 py-2">
+                <table class="min-w-full">
+                    <thead class="bg-gray-50">
+                        <tr class="text-center">
+                            <h2>Project name: {{ props.project.name }}</h2>
 
-                <h1>{{ props.project.name }}</h1>
+                            <h1>The Project is: {{ props.project.status.name }}</h1>
 
-                <h1>Status</h1>
+                            <h1>Afdeling: </h1>
 
-                <h1>Afdeling</h1>
+                            <h1>Projektejer: </h1>
 
-                <h1>Information</h1>
+                            <textarea>Information</textarea>
 
-                <h1>Upload</h1>
+                                <div>
+                                    <h1>FOLDERSTRUKTUR</h1>
+                                </div>
 
-                <h1>Delete</h1>
+                            <div class="align-middle">
+                                <button
+                                    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold m-2 align-middle
+                                    hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                    <Link href="/projects">Upload to Project</Link>
+                                </button>
 
-                    <button
-                        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold m-2 align-middle
-                        hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-                        <Link href="/projects">Go back</Link>
-                    </button>
+                                <button
+                                    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold m-2 align-middle
+                                    hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                    <Link href="/projects">Delete the Project</Link>
+                                </button>
 
+                                <button
+                                    class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold m-2 align-middle
+                                    hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                                    <Link href="/projects">Go back</Link>
+                                </button>
+
+                            </div>
+                        </tr>
+                    </thead>
+                </table>
             </div>
         </div>
     </div>
