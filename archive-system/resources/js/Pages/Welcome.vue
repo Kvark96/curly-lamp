e6bf4468087f1cae72a447cebd8a36ce2931146c
Oliver Juul Reder <oliver.j.reder@gmail.com>
2022-12-12 16:34:00 +0100

    Fixed the login screen, so its properly centered

--- archive-system/resources/js/Pages/Welcome.vue
+++ archive-system/resources/js/Pages/Welcome.vue
@@ -9,26 +9,35 @@ defineProps({
 </script>
 
 <template>
-
     <Head title="Welcome" />
-        <div v-if="$page.props.auth.user"></div>
-        <div v-else class="fixed inset-1/4">
+    <div v-if="$page.props.auth.user"></div>
+    <div v-else class="fixed inset-1/4">
+
+        <div>
             <img class="object-scale-down h-120 w-240" src="../../assets/SDC_logo_rgb.png" />
         </div>
 
         <div v-if="canLogin">
-            <Link v-if="$page.props.auth.user" :href="route('projects.index')"
-            class="fixed inset-1/4">
-            <img src="../../assets/SDC_logo_rgb.png"
-            />
-            </Link>
+            <div class="grid grid-cols-2 w-full">
+                <div class="col-span-2" v-if="$page.props.auth.user">
+                    <Link :href="route('projects.index')" class="fixed inset-1/4">
+                    <img src="../../assets/SDC_logo_rgb.png" />
+                    </Link>
+                </div>
 
-            <div v-else class="fixed inset-x-1/3 px-52 py-40">
-                <button class="bg-transparent hover:bg-cyan-600 text-cyan-600 font-semibold m-2 hover:text-white
-                    py-1 px-1 border border-cyan-600 hover:border-transparent rounded"><Link :href="route('login')">Log in</Link></button>
-                <button v-if="canRegister" class="bg-transparent hover:bg-cyan-900 text-cyan-900 font-semibold m-2 hover:text-white
-                    py-1 px-1 border border-cyan-900 hover:border-transparent rounded"><Link :href="route('register')">Register</Link></button>
+                <div v-else class="col-span-2 flex justify-center py-10">
+
+                    <Link :href="route('login')" as="button" type="button" class="w-1/3 bg-transparent hover:bg-cyan-600 text-cyan-600 font-semibold m-2 hover:text-white
+                    py-1 px-1 border border-cyan-600 hover:border-transparent rounded">Log in</Link>
+
+
+                    <Link v-if="canRegister" :href="route('register')" as="button" type="button" class="w-1/3 bg-transparent hover:bg-cyan-900 text-cyan-900 font-semibold m-2 hover:text-white
+                py-1 px-1 border border-cyan-900 hover:border-transparent rounded">Register</Link>
+
+
+                </div>
             </div>
         </div>
+    </div>
 
 </template>
